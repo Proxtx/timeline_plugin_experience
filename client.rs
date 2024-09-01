41fd30aee519ce98b48fdda03a0cc522443fd10d
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-09-01 18:20:03 +0200

    now able to create experiences

--- client.rs
+++ client.rs
@@ -1,8 +1,7 @@
 use { 
-    leptos::{view, IntoView, View, Callback, window}, 
-    serde::{Deserialize, Serialize},
-    crate::plugin_manager::PluginData,
-    experiences_navigator_lib::{api::relative_url, wrappers::Band}
+    leptos::{view, IntoView, View, Callback, window, html::Input, spawn_local}, 
+    crate::{plugin_manager::PluginData, wrappers::StyledView},
+    experiences_navigator_lib::{api::relative_url, wrappers::Band, experiences_types::types::{CompressedExperienceEvent, CreateExperienceRequest}}
 };
 
 pub struct Plugin {
@@ -20,26 +19,82 @@ impl crate::Plugin for Plugin {
     }
 
     fn get_component(&self, data: crate::plugin_manager::PluginEventData) -> crate::plugin_manager::EventResult<Box<dyn FnOnce() -> leptos::View>> {
-        let data = data.get_data::<String>()?;
+        let data = data.get_data::<CompressedExperienceEvent>()?;
         Ok(Box::new(move || -> View {
-            view! {
-                <div style="display: flex; flex-direction: column;">
-                    <img
-                        style="width: 100%"
-                        src=relative_url(&format!("/api/experience/{}/cover/big", data))
-                            .unwrap()
-                            .to_string()
-                    />
-                    <Band on:click=Callback::new(move |_| {
-                        window()
-                            .location()
-                            .set_href(
-                                &relative_url(&format!("/experience/{}", data)).unwrap().to_string(),
-                            )
-                            .unwrap();
-                    })>Open</Band>
-                </div>
-            }.into_view()
+            match data {
+                CompressedExperienceEvent::Experience(id) => {
+                    view! {
+                        <div style="display: flex; flex-direction: column;">
+                            <img
+                                style="width: 100%"
+                                src=relative_url(&format!("/api/experience/{}/cover/big", id))
+                                    .unwrap()
+                                    .to_string()
+                            />
+                            <Band on:click=Callback::new(move |_| {
+                                window()
+                                    .location()
+                                    .set_href(
+                                        &relative_url(&format!("/experience/{}", id))
+                                            .unwrap()
+                                            .to_string(),
+                                    )
+                                    .unwrap();
+                            })>Open</Band>
+                        </div>
+                    }.into_view()
+                }
+                CompressedExperienceEvent::Create(timing) => {
+                    let name_ref = leptos::create_node_ref::<Input>();
+
+                    view! {
+                        <style>
+                            "
+                            .name_input {
+                             border: none;
+                             width: 100%;
+                             box-sizing: border-box;
+                             background-color: var(--accentColor1);
+                             padding: var(--contentSpacing);
+                             color: var(--lightColor);
+                            }
+                            .name_input::placeholder{
+                             color: var(--lightColor);
+                            }
+                            .name_input:focus{
+                             outline: none;
+                            }"
+                        </style>
+                        <StyledView>
+                            <input ref=name_ref class="name_input" placeholder="Name"/>
+                            <Band click=Callback::new(move |_| {
+                                let timing = timing.clone();
+                                spawn_local(async move {
+                                    match crate::api::api_request(
+                                            "/plugin/timeline_plugin_experience/create",
+                                            &CreateExperienceRequest {
+                                                name: name_ref.get().unwrap().value(),
+                                                time: timing.clone(),
+                                            },
+                                        )
+                                        .await
+                                    {
+                                        Ok(()) => {
+                                            let _ = window().location().reload();
+                                        }
+                                        Err(e) => {
+                                            let _ = window()
+                                                .alert_with_message(
+                                                    &format!("Unable to create Experience: {}", e),
+                                                );
+                                        }
+                                    }
+                                })
+                            })>Create</Band>
+                        </StyledView>
+                    }.into_view()
+                }
+            }
         }))
     }
 
--- server.rs
+++ server.rs
@@ -1,21 +1,45 @@
 use {
-    crate::PluginData,
-    chrono::{DateTime, TimeDelta},
+    crate::{api::auth, db::Event, Database, PluginData},
+    chrono::TimeDelta,
+    experiences_types_lib::types::{CompressedExperienceEvent, CreateExperienceRequest},
+    futures::StreamExt,
+    rocket::{
+        http::Status,
+        post,
+        response::status::{self, Custom},
+        routes, Build, Rocket,
+    },
     serde::Deserialize,
-    serde_json::json,
-    types::{api::CompressedEvent, timing::TimeRange},
-    url::Url
+    types::{
+        api::{APIError, APIResult, AvailablePlugins, CompressedEvent},
+        timing::Timing,
+    },
+    url::Url,
 };
 
-#[derive(Deserialize)]
-struct ConfigData{
+use serde::Serialize;
+
+use crate::config::Config;
+use rocket::http::CookieJar;
+use rocket::serde::json::Json;
+use rocket::State;
+use std::sync::Arc;
+
+#[derive(Deserialize, Clone)]
+struct ConfigData {
     pub password: String,
-    pub url: Url
+    pub url: Url,
 }
 
 pub struct Plugin {
-    _plugin_data: PluginData,
-    config: ConfigData
+    plugin_data: PluginData,
+    config: ConfigData,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DatabaseExperience {
+    name: String,
+    id: String,
 }
 
 impl crate::Plugin for Plugin {
@@ -29,7 +53,10 @@ impl crate::Plugin for Plugin {
         )
         .unwrap_or_else(|e| panic!("Unable to init experiences plugin! Provided config does not fit the requirements: {}", e));
 
-        Plugin { _plugin_data: data, config }
+        Plugin {
+            plugin_data: data,
+            config,
+        }
     }
 
     fn get_type() -> types::api::AvailablePlugins
@@ -39,9 +66,130 @@ impl crate::Plugin for Plugin {
         types::api::AvailablePlugins::timeline_plugin_experience
     }
 
-    fn get_compressed_events (&self, query_range: &types::timing::TimeRange) -> std::pin::Pin<Box<dyn futures::Future<Output = types::api::APIResult<Vec<types::api::CompressedEvent>>> + Send>> {
+    fn get_routes() -> Vec<rocket::Route>
+    where
+        Self: Sized,
+    {
+        routes![create_experience]
+    }
+
+    fn rocket_build_access(&self, rocket: Rocket<Build>) -> Rocket<Build> {
+        rocket.manage(self.config.clone())
+    }
+
+    fn get_compressed_events(
+        &self,
+        query_range: &types::timing::TimeRange,
+    ) -> std::pin::Pin<
+        Box<
+            dyn futures::Future<Output = types::api::APIResult<Vec<types::api::CompressedEvent>>>
+                + Send,
+        >,
+    > {
+        let database = self.plugin_data.database.clone();
+        let query_range = query_range.clone();
+        let filter = Database::generate_range_filter(&query_range);
+        let plg_filter =
+            Database::generate_find_plugin_filter(AvailablePlugins::timeline_plugin_experience);
+        let filter = Database::combine_documents(filter, plg_filter);
         Box::pin(async move {
-            Ok(Vec::new())
+            let mut cursor = database
+                .get_events::<DatabaseExperience>()
+                .find(filter, None)
+                .await?;
+            let mut result = Vec::new();
+            while let Some(v) = cursor.next().await {
+                let t = v?;
+                result.push(CompressedEvent {
+                    title: t.event.name,
+                    time: t.timing,
+                    data: Box::new(CompressedExperienceEvent::Experience(t.event.id)),
+                })
+            }
+
+            let mut current = query_range.start;
+
+            while current < query_range.end {
+                let new_current = current
+                    .checked_add_signed(TimeDelta::try_hours(1).unwrap())
+                    .unwrap();
+                let timing = Timing::Instant(current);
+                result.push(CompressedEvent {
+                    title: "Create Experience".to_string(),
+                    time: timing.clone(),
+                    data: Box::new(CompressedExperienceEvent::Create(timing)),
+                });
+                current = new_current;
+            }
+
+            Ok(result)
         })
+    }
+}
+
+#[post("/create", data = "<request>")]
+async fn create_experience(
+    request: Json<CreateExperienceRequest>,
+    cookies: &CookieJar<'_>,
+    config: &State<Config>,
+    database: &State<Arc<Database>>,
+    experience_config: &State<ConfigData>,
+) -> Custom<Json<APIResult<()>>> {
+    if auth(cookies, config).is_err() {
+        return status::Custom(
+            Status::Unauthorized,
+            Json(Err(APIError::AuthenticationError)),
+        );
+    }
+
+    let client = reqwest::Client::new();
+    let id = match client.post(experience_config.url.join("/api/experience/create").unwrap()).header(reqwest::header::COOKIE, format!("pwd={}", experience_config.password)).body(serde_json::to_string(&request.0).unwrap()).send().await {
+        Ok(v) => {
+            match v.text().await {
+                Ok(v) => {
+                    match serde_json::from_str::<APIResult<String>>(&v) {
+                        Ok(v) => match v {
+                            Ok(v) => v,
+                            Err(e) => {
+                                return status::Custom(Status::InternalServerError, Json(Err(APIError::SerdeJsonError(format!("Unable to create experience on experiences server: The experiences server reported an error: {}", e)))))
+                            }
+                        },
+                        Err(e) => {
+                            return status::Custom(Status::InternalServerError, Json(Err(APIError::SerdeJsonError(format!("Unable to read response from experiences server: {}", e)))));
+                        }
+                    }
+                },
+                Err(e) => {
+                    return status::Custom(Status::InternalServerError, Json(Err(APIError::Custom(format!("Unable to read response from experiences server: {}", e)))));
+                }
+            }
+        }
+        Err(e) => {
+            return status::Custom(Status::InternalServerError, Json(Err(APIError::Custom(format!("Unable to send request to experiences server: {}", e)))));
+        }
+    };
+
+    match database
+        .register_single_event(&Event {
+            timing: request.time.clone(),
+            id: id.clone(),
+            plugin: <Plugin as crate::Plugin>::get_type(),
+            event: DatabaseExperience {
+                name: request.name.clone(),
+                id,
+            },
+        })
+        .await
+    {
+        Ok(_) => status::Custom(Status::Ok, Json(Ok(()))),
+        Err(e) => {
+            crate::error::error(
+                database.inner().clone(),
+                &e,
+                Some(<Plugin as crate::Plugin>::get_type()),
+                &config.error_report_url,
+            );
+            status::Custom(Status::InternalServerError, Json(Err(e.into())))
+        }
     }
 }
